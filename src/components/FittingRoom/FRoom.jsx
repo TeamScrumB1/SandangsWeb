677c1a4dcdb8ac2260d367ad602f95e5c2b3d0db
daniasupriyadi <daniasupriyadi2106@gmail.com>
2022-10-14 13:41:06 +0700

    FittingRoom

--- src/components/FittingRoom/FRoom.jsx
+++ src/components/FittingRoom/FRoom.jsx
@@ -3,45 +3,14 @@ import FittingRoom from "../FittingRoom/Froom.css"
 import ButtonFroom from "../discount/ButtonFroom"
 import { Card } from "react-bootstrap"
 
-const Froom = () => {
-  const cardInfo = [
-    { 
-      image: "public\images\category\jaket.png", 
-      tittle: "jaket", 
-      text: "ini jaket"
-    },
-    { 
-      image: "public\images\category\kaos.png", 
-      tittle: "kaos", 
-      text: "ini kaos"
-    },
-    { 
-      image: "public\images\category\kemeja.png", 
-      tittle: "kemeja", 
-      text: "ini kemeja"
-    },
-    { 
-      image: "public\images\category\jeans.png", 
-      tittle: "celana", 
-      text: "ini celana"
-    },
-  ];
-
-  const renderCard = (card, index) => {
-    return(
-      <Card style={{width: "18rem" }} key={index} className="box">
-        <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
-        <Card.Body>
-          <Card.Tittle>{card.tittle}</Card.Tittle>
-          <Card.Text>{card.text}</Card.Text>
-        </Card.Body>
-      </Card>
-    )
-  }
-
-  return (
-    <div className="grid">{cardInfo.map(renderCard)}</div>
+const Froom = props =>{
+  return(
+    <div className="card-text-center">
+      <div className="overflow">
+        <img src={{}} alt=""/>
+      </div>
+    </div>
   );
-};
+}
 
 export default Froom
