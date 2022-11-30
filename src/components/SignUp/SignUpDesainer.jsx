164d90a2c527316193c68cbebf6cf305ac8515f0
aazzizzz1 <azizvbi33@gmail.com>
2022-11-30 19:38:29 +0700

    Commit Post

--- src/components/SignUp/SignUpDesainer.jsx
+++ src/components/SignUp/SignUpDesainer.jsx
@@ -1,11 +1,118 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-class SignUpDesainer extends Component {
-  render() {
+function PostApi() {
+  const url = "http://api.yufagency.com/signup_desainer";
+  const [postdata, setPostdata] = useState([]);
+
+
+  // const getDataPost = async () => {
+  //   const response = await fetch(url);
+  //   const dataPost = await response.json();
+  //   setPostdata(dataPost);
+  //   console.log(postdata);
+  // };
+
+
+  useEffect(() => {
+    getDataPost();
+  }, []);
+
+  async function getDataPost()
+  {
+    let result = await fetch("http://api.yufagency.com/signup_desainer");
+    result = await result.json();
+    setPostdata(result.reverse())
+    console.log(result);
+    
+  }
+
+  return (
+    <div className="Post">
+      {postdata.map((item) => {
+        return (
+            <CardPost 
+                key={item.IdPost} 
+                idPost={item.IdPost}
+                id={item.IdPengepost} 
+                nama={item.NamaPengepost} 
+                judul={item.JudulPost} 
+                isi={item.IsiPost} 
+                tgl={item.created_at} 
+                image={item.img_path}
+                 />
+        );
+      })}
+    </div>
+  );
+};
+
+
+function SignUpDesainer() {
+
+  const url = "http://api.yufagency.com/signup_desainer";
+  const [data, setData] = useState({
+    nama: "",
+    email: "",
+    password: "",
+    alamat: "",
+    foto_ktp: "",
+    nama_desainer: "",
+    npwp: "",
+    foto_logo: "",
+    link_portofolio: "",
+  });
+
+  const formData = new FormData();
+  formData.append("nama", data.nama);
+  formData.append("email", data.email);
+  formData.append("password", data.password);
+  formData.append("alamat", data.alamat);
+  formData.append("foto_ktp", data.foto_ktp);
+  formData.append("nama_desainer", data.nama_desainer);
+  formData.append("npwp", data.npwp);
+  formData.append("foto_logo", data.foto_logo);
+  formData.append("link_portofolio", data.link_portofolio);
+
+  function handle(e) {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  }
+
+  function handlefile(e) {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.files[0];
+    setData(newData);
+    console.log(newData);
+  }
+
+  function submit(e) {
+    e.preventDefault();
+    axios
+      .post(url, {
+        nama: data.nama,
+        email: data.email,
+        password: data.password,
+        alamat: data.alamat,
+        foto_ktp: data.foto_ktp,
+        nama_desainer: data.nama_desainer,
+        npwp: data.npwp,
+        foto_logo: data.foto_logo,
+        link_portofolio: data.link_portofolio,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+
+  //axios post apend
+
     return (
-      <form>
+      <form onSubmit={(e) => submit(e)}>
         <div class="image_signup">
           <img src="././images/sandangs/sandangsbiru.png" alt=""></img>
         </div>
@@ -16,6 +123,9 @@ class SignUpDesainer extends Component {
             type="text"
             className="form-control"
             placeholder="Nama anda"
+            name="nama"
+            id="nama"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="mb-3">
@@ -24,6 +134,9 @@ class SignUpDesainer extends Component {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            name="email"
+            id="email"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="mb-3">
@@ -32,6 +145,9 @@ class SignUpDesainer extends Component {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            name="password"
+            id="password"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="mb-3">
@@ -40,11 +156,18 @@ class SignUpDesainer extends Component {
             type="text"
             className="form-control"
             placeholder="Alamat anda"
+            name="alamat"
+            id="alamat"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="mb-3">
           <label>Foto KTP</label>
-          <input type="file" className="form-control" placeholder="Foto KTP" />
+          <input type="file" className="form-control" placeholder="Foto KTP" 
+          name="foto_ktp"
+          id="foto_ktp"
+          onChange={(e) => handlefile(e)}
+          />
         </div>
         <div className="mb-3">
           <label>Nama Desainer</label>
@@ -52,6 +175,9 @@ class SignUpDesainer extends Component {
             type="text"
             className="form-control"
             placeholder="Nama Desainer"
+            name="nama_desainer"
+            id="nama_desainer"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="mb-3">
@@ -60,6 +186,9 @@ class SignUpDesainer extends Component {
             type="text"
             className="form-control"
             placeholder="Nomor Pokok Wajib Pajak (NPWP)"
+            name="npwp"
+            id="npwp"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="mb-3">
@@ -68,6 +197,9 @@ class SignUpDesainer extends Component {
             type="file"
             className="form-control"
             placeholder="Logo / "
+            name="logo_banner"
+            id="logo_banner"
+            onChange={(e) => handlefile(e)}
           />
         </div>
         <div className="mb-3">
@@ -76,10 +208,13 @@ class SignUpDesainer extends Component {
             type="url"
             className="form-control"
             placeholder="Link Portofolio"
+            name="link_portofolio"
+            id="link_portofolio"
+            onChange={(e) => handle(e)}
           />
         </div>
         <div className="d-grid">
-          <Link to="/">
+          <Link to="/">            
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
@@ -90,7 +225,7 @@ class SignUpDesainer extends Component {
         </p>
       </form>
     );
-  }
 }
 
 export default SignUpDesainer;
+
